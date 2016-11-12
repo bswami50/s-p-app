9b253a450ac69a53b22630022be0ae2adf57fadd
bswami50 <bswami50@users.noreply.github.com>
2016-11-11 20:51:38 -0500

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -65,7 +65,9 @@ class WelcomeController < ApplicationController
        
       if(!data["tracks"][0]["composer"].blank?) #sanity check for tracks
        data["tracks"].each_with_index do |record, i|
-        file_name = record["kriti"]+"-"+record["ragam"]
+        file_name = record["kriti"]
+        file_name = file_name.split('_').join(' ').downcase.gsub(' ', '_')
+        file_name = filename + "-" + record["ragam"]
         final_file_path = record["url1"]
         $file_path_array.push(final_file_path)
         $file_name_array.push(file_name)
