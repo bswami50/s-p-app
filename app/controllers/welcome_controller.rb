20992982eedf86dfda1f194fa2e72020e17bf745
bswami50 <bswami50@users.noreply.github.com>
2016-11-25 21:03:10 -0500

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -146,7 +146,7 @@ class WelcomeController < ApplicationController
           response = HTTParty.get(url, :headers => {"User-Agent" => "#{@user_agent}"}, follow_redirects: false)
           data = JSON.parse(response.body)
  
-          puts data
+          puts data.sort_by { |k, v| v[:trackcount] }
           
           data.each_with_index do |record, i|
             krithi = record["kriti"].split('_').join(' ').downcase.gsub(' ', '_')
