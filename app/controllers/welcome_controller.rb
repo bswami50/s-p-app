28cfb8a5e88273ecada273acad6edb5103a51282
bswami50 <bswami50@users.noreply.github.com>
2016-11-29 15:36:53 -0500

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,6 +12,7 @@ $full_array = Hash.new{|hsh,key| hsh[key] = [] }
 NUM_FIELDS = 7
 @user_agent = "SangeethaPriya/1.1.5 CFNetwork/808.0.2 Darwin/16.0.0"
 @user_agent_play = "AppleCoreMedia/1.0.0.14A456 (iPhone; U; CPU OS 10_0_2 like Mac OS X; en_us)"
+@ragas = YAML.load_file('raga.yml')
 
 class WelcomeController < ApplicationController
   skip_before_action :verify_authenticity_token
