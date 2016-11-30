ac33376fe03507c97401b665a0fe0e0f8de35131
bswami50 <bswami50@users.noreply.github.com>
2016-11-29 22:53:56 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,11 @@ Rails.application.routes.draw do
   get  "/searchKrithi"         => "welcome#clearData"
   get  "/searchArtist"         => "welcome#clearData"
   get  "/searchRagam"          => "welcome#clearData" 
-  get  "/searchKrithiFuzzy"    => "welcome#clearData"
   get  "/clearData"            => "welcome#clearData"
   
   post "searchConcert"         => "welcome#searchConcert"
   post "searchKrithi"          => "welcome#searchKrithi"
   post "searchRagam"           => "welcome#searchRagam" 
   post "searchArtist"          => "welcome#searchArtist"
-  post "searchKrithiFuzzy"     => "welcome#searchKrithiFuzzy" 
   post "clearData"             => "welcome#clearData"               
 end
